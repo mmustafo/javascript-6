28e6864de4dbf170b2cf12565c0a3af1211534d6
mustafo <mustafo@gmail.com>
2025-01-23 15:55:50 +0500

    in coment

---
+++ app.js
@@ -0,0 +1,144 @@
+// for 5
+// let a = 12
+// let b = 21
+// let counter = 0
+// for (let i =a; i<=b; i++){
+//      counter+=i
+// }
+// console.log(counter);
+// for 6
+// let a = 12
+// let b = 21
+// let counter = 1
+// for (let i =a; i<=b; i++){
+//      counter*=i
+// }
+// console.log(counter);
+// for 7
+// let a = 12
+// let b = 21
+// let counter = 0
+// for (let i =a; i<=b; i++){
+//      counter+=i**2
+// }
+// console.log(counter);
+// for 8
+// let n = 5
+// let counter = 0
+// for (let i=1; i<=n; i++){
+//      counter+=1/i
+// }
+// console.log(Math.round(counter));
+// for 9
+// let n = 2
+// let counter = 1
+// for (let i=1.1; i<=n; i=i+0.1){
+//      counter*=i
+// }
+// console.log(Math.round(counter));
+// for 10
+// let n = 9
+// let counter = 0
+// for (let i=1; i <=n; i++) {
+//      counter+=2*i-1
+//      console.log(counter);
+// }
+
+// for 11
+// let n = 12
+// let a =2;
+// let counter = 1
+// for (let i=1; i<=n; i++){
+//      counter*=a
+// }
+// console.log(counter);
+// for 12
+// let n = 12
+// let a =2;
+// let counter = 1
+// for (let i=1; i<=n; i++){
+//      counter*=a
+//      console.log(counter);
+// }
+// for 13
+// let a = 12
+// let n = 23
+// let counter = 0
+// for (let i=1; i<=n; i++){
+//      counter+=(a**2+n)
+//      console.log(counter);
+// }
+// for 17
+// let a = 11
+// let b = 22
+// for (let i =a; i<=b; i++){
+// for (let j=1; j<=i; j++){
+
+//   console.log(i);
+// }
+// }
+// for 18
+// let n = 12
+// let counter = 0
+// for (let i = 0; i<=n; i++){
+//      if(n%i==0){
+//           console.log(i);
+//           counter++
+//      }
+// }
+// console.log(` ta = ${counter} boluvchi`);
+// for 19
+// let n = 11
+// for (let i = 1; i<=n; i++){
+//      if(n%2==1){
+//           console.log(`ha buson ${n} tub`);
+//      } else{
+//           console.log(`bu son ${n} tub emas`);
+//      }
+// }
+
+// for 20
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += j + " ";
+//   }
+//   console.log(row.trim());
+// }
+
+// while 1
+// let a = 12
+// let b = 5;  
+// if(a>b){
+//      let counter = a
+//      while(counter>=b){
+//           counter-=b
+//      }
+//      console.log(`A ${a} uzunlikdagi kesmada malsimal  ${b} bo'sh qismi: ${counter}`);
+// }
+
+// while 2
+// let a = 13;
+// let b = 3;
+// let s = b;
+// let counter = 0;
+// while (s < a) {
+//   s += b
+//   counter++;
+// }
+// console.log(counter);
+// while 10
+// let n = 4
+// if (n > 1) {
+//      for (let i=0; i<=n; i++){
+//           if (n % 2==1){
+//                console.log("bu son tub");
+               
+//           }else{
+//                console.log("bu son tub emas");
+//           }
+//      }
+// }else{
+//      console.log("hato");
+// }
